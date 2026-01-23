82c92983427e684e45419fa68d1381aa610f5181
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-23 17:14:27 +0800

    finished 07revere finished 08remove
    need to lean .forEach and includes array method

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,38 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
+  for (let x = 0; x < array.length; x++) {
+    let item = array[x];
+    toRemove = false;
+
+    for (let i = 0; i < args.length; i++) {
+      if (item === args[i]) {
+        toRemove = true;
+        break;
+      }
+    }
+
+    if (toRemove === false) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
 };
+
+// filter + includes
+// const removeFromArray = (array, ...args) => {
+//   return array.filter(item => !args.includes(item));
+// };
+
+// forEach() + includes()
+// const removeFromArray = (array, ...args) => {
+//   const newArray = [];
+//   array.forEach(item => {
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
