b0d45d0300aa4154b2dbb9c5683231166ab65048
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-01-23 17:40:47 +0800

    finished 09sumall
    couldve used a more efficient solut

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
 
+  const toSum = [];
+
+  if (num1 > num2) {
+    for (let i = num1; i >= num2; i--) {
+      toSum.push(i);
+    }
+  } else {
+    for (let i = num1; i <= num2; i++) {
+      toSum.push(i);
+    }
+  }
+
+  const total = toSum.reduce((prev, curr) => prev + curr, 0);
+  return total;
 };
 
 // Do not edit below this line
