90ae976ba6368dd856445202c40356088e06f80c
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-02-01 20:39:20 +0800

    finished 13 palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const clean = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
 
+  const reverse = clean.split("").reverse().join("").toLowerCase();
+  if (reverse === clean) return true;
+  else return false;
 };
 
 // Do not edit below this line
