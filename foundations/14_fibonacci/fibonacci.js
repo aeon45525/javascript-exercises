625f5d2acfd7ace839d07194c56f74caf34c83ef
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-02-02 19:08:44 +0800

    finished 14_fibonacci solution was better

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num === "string") num = Number(num);
 
+  if (num === 1 || num === 2) return 1;
+  else if (num === 0) return 0;
+  else if (num < 0) return "OOPS";
+
+  let prev = 1;
+  let current = 1;
+  let next = 0;
+  for (let i = 3; i <= num; i++) {
+    next = prev + current;
+    prev = current;
+    current = next;
+  }
+  return current;
 };
 
 // Do not edit below this line
